52b0b8f5584cfb17a10fa50c4dc0efe818191bd6
priyanshu <58025838+palpriyanshu@users.noreply.github.com>
2019-11-27 13:28:42 +0530

    readTransaction file is modified
    two functions are added
    extractTransactions on the basis of empid is working
    and tested also

--- src/readTransactions.js
+++ src/readTransactions.js
@@ -5,4 +5,17 @@ const fetchTransactions = function(path, fileSys) {
   return JSON.parse(fetchedTransaction);
 };
 
+const isCriteriaTrue = function(creterionValue) {
+  return function(transaction) {
+    const empid = transaction["empid"] == creterionValue;
+    const date = transaction["date"] == creterionValue;
+    return empid || date;
+  };
+};
+
+const extractTransactions = function(fetchedTransactions, criterionValue) {
+  return fetchedTransactions.filter(isCriteriaTrue(criterionValue));
+};
+
 exports.fetchTransactions = fetchTransactions;
+exports.extractTransactions = extractTransactions;
--- test/testReadTransaction.js
+++ test/testReadTransaction.js
@@ -2,6 +2,7 @@ const read = require("../src/readTransactions.js");
 const assert = require("assert");
 
 const fetchTransactions = read.fetchTransactions;
+const extractTransactions = read.extractTransactions;
 
 describe("fetchTransactions", function() {
   it("should fetch the transactions from file when file is exist", function() {
@@ -17,5 +18,21 @@ describe("fetchTransactions", function() {
       }
     };
     assert.deepStrictEqual(fetchTransactions(path, fileSysc), []);
+  });
+});
+
+describe("extractTransactions", function() {
+  it("should extract the transactions on the basis of empid", function() {
+    let fetchedTransactions = [
+      { empid: 1, date: "2 / 3 / 2000" },
+      { empid: 2, date: "2 / 3 / 2000" },
+      { empid: 1, date: "1 / 4 / 1999" }
+    ];
+    let empid = 1;
+    let creteria = empid;
+    assert.deepStrictEqual(extractTransactions(fetchedTransactions, creteria), [
+      { empid: 1, date: "2 / 3 / 2000" },
+      { empid: 1, date: "1 / 4 / 1999" }
+    ]);
   });
 });
